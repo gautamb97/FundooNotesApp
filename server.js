370f42082ba681ad36be50808833045c3d40fa06
gautam971997 <www.gautam971997@gmail.com>
2021-04-23 18:54:09 +0530

    refactor: Add DataBase configuration To Connect With MongoDB

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 /* eslint-disable linebreak-style */
 const express = require('express');
 const logger = require('./app/logger/user');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+
+require('./config/dbConfig');
+
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the FundooNotesApp.' });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   logger.log('info', 'Server is listening on port 3000');
 });
