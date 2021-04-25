57eb1163ae3f7148dc7200b18e81bba51f047582
gautam971997 <www.gautam971997@gmail.com>
2021-04-25 15:38:10 +0530

    refactor: Schema Validation For The Fields We Have

--- app/models/user.js
+++ app/models/user.js
@@ -2,10 +2,10 @@
 const mongoose = require('mongoose');
 
 const fundooNoteSchema = mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, requires: true },
+  password: { type: String, required: true },
 }, {
   timestamps: true,
 });
