0a5c90f3d40ddf3fe641bd9dbe916a0ae076501a
gautam971997 <www.gautam971997@gmail.com>
2021-05-06 16:38:01 +0530

    test: given improper registration details should not save in db

--- test/registration-test.js
+++ test/registration-test.js
@@ -19,4 +19,15 @@ describe('registartion', () => {
         done();
       });
   });
+  it('givenRegistrationDetails_whenProper_shouldSaveInDB', (done) => {
+    const registartionDetails = registrationData.user.registration;
+    chai
+      .request(server)
+      .post('/registration')
+      .send(registartionDetails)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
 });
