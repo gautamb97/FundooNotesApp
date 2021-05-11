8b2e33240d23a6ab21e24cd880d710fe83dac417
gautam971997 <www.gautam971997@gmail.com>
2021-05-11 19:50:40 +0530

    test: create note when data is proper

---
+++ test/notes-test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+chai.use(chaiHttp);
+const noteData = require('./notes.json');
+
+chai.should();
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Ind3dy5nYXV0YW05NzE5OTdAZ21haWwuY29tIiwiaWQiOiI2MDk3YTU1MjNmYTUwZDAxYTRhNTY0N2IiLCJpYXQiOjE2MjA3NDI2ODMsImV4cCI6MTYyMDgyOTA4M30.6IGiHWT-Hvx17UprOvCbla4LpzRfs2FQftmBWAdwMpI';
+
+describe('notes', () => {
+  it('givenNoteDetails_whenProper_shouldAbleToCreateANote', (done) => {
+    const noteDetails = noteData.notes.createNote;
+    chai
+      .request(server)
+      .post('/notes')
+      .set('token', `${token}`)
+      .send(noteDetails)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
