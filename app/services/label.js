6646faed0b22898f0c2c187ebb2c7480815befbf
gautam971997 <www.gautam971997@gmail.com>
2021-05-13 20:49:44 +0530

    feat: create label for particular user by creating promise object

---
+++ app/services/label.js
@@ -0,0 +1,22 @@
+/**
+ * @description   : It is work as a middleware between models and controller
+ * @file          : label.js
+ * @author        : Gautam Biswal <gautam971997@gmail.com>
+*/
+const models = require('../models/label');
+
+class Service {
+/**
+ * @description   : It is used to create label taking data from controller and sending to models
+ * @param {data}  : it contains data which we are passing from body
+ * @param {token} : its has login token and sending to helper to extract id of user
+*/
+  createLabel = (data) => new Promise((resolve, reject) => {
+    if (data) {
+      return resolve(models.createLabel(data));
+    }
+    return reject(error, 'label not created');
+  });
+    //models.create(data, callback);
+}
+module.exports = new Service();
