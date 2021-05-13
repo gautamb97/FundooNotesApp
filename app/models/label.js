8e876eb1668352aa47ba5575cddb89f9b7506206
gautam971997 <www.gautam971997@gmail.com>
2021-05-13 20:50:52 +0530

    feat: create schema for label

---
+++ app/models/label.js
@@ -0,0 +1,32 @@
+/**
+ * Executaion       :1.default node      cmd>nodemon start
+ *
+ * purpose          :to save find update and delete in the database
+ *
+ * @file            :label.js
+ * @author          :Gautam Biswal
+ * @version         :1.0.0
+*/
+const { reject } = require('bluebird');
+const mongoose = require('mongoose');
+
+const labelSchema = mongoose.Schema({
+  label: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+}, {
+  timestamps: true, versionKey: false,
+});
+
+const LabelModel = mongoose.model('Label', labelSchema);
+
+class Model {
+    createLabel = (data) => {
+      const label = new LabelModel({
+        label: data.label,
+        userId: data.userId,
+      });
+      label.save();
+    }
+}
+
+module.exports = new Model();
