9614673f7ed631f505aa5c7e3dde137469bf68f6
gautam971997 <www.gautam971997@gmail.com>
2021-05-14 14:56:38 +0530

    refactor: comments for update label

--- app/services/label.js
+++ app/services/label.js
@@ -18,6 +18,11 @@ class Service {
     return reject(error, 'label not created');
   });
 
+  /**
+ * @description   : It is used to update label taking data from controller and sending to models
+ * @param {data}  : it contains data which we are passing from body
+ * @param {token} : its has login token and sending to helper to extract id of user
+*/
   updateLabel = (data) => new Promise((resolve, reject) => {
     const result = models.updateLabel(data);
     result.then((labelData) => {
